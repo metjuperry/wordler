aad1998cfc2217ead62513b49e858296babc1e70
metjuperry <matej.samler@seznam.cz>
2026-05-07 22:53:00 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ export default function App() {
   return (
     <div className="app">
       <header className="header">
-        <h1>Wordler</h1>
+        <h1>Metju.Wordler</h1>
         <p className="tagline">Cheat at Wordle, responsibly.</p>
       </header>
 
